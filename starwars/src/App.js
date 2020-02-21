001908c4bd4e50ada038d5b8c16a44e608396afb
Charlie DiFranco <charlie.difranco2@gmail.com>
2020-02-21 12:55:48 -0600

    added pagination but not working correctly

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,22 +2,34 @@ import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import CardContainer from './components/CardContainer';
 import SearchBar from "./components/SearchBar";
+import Pagination from "./components/Pagination";
 import './App.css';
-import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 const App = () => {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    axios("https://swapi.co/api/people/")
+    axios(`https://swapi.co/api/people/?page=${page}`)
     .then(res => { 
       setData(res.data.results)
       console.log(res)})
-  }, [])
+  }, [page])
 
   const searchHandler = (e) => {
     setSearchTerm(e.target.value)
+  };
+
+  const pageHandler = (e) => {
+    console.log(e.target.value)
+    if (e.target.value == "next") {
+      setPage(page + 1)
+      e.preventDefault()
+    } else {
+      setPage(page - 1)
+      e.preventDefault();
+    }
   };
 
   return (
@@ -30,6 +42,11 @@ const App = () => {
       <CardContainer 
         people={data}
         searchTerm={searchTerm}
+      />
+      <Pagination 
+        onClick={pageHandler}
+        value={{next: "next",
+                previous: "previous"}}
       />
     </div>
   );
